84baa28b151ffefd8ece9d89bb2f16bc3e28e909
Hyunsung <chs52699@naver.com>
2023-07-10 13:29:53 +0900

    test

--- javaStudy/Study/Main.java
+++ javaStudy/Study/Main.java
@@ -1,14 +1,17 @@
-import java.util.Arrays;
-
 public class Main {
-    public static void main(String[] args){
-        int[] arr = new int[]{20,51,23,56,78};
-        String arr_toString = Arrays.toString(arr);
-        String repl = arr_toString.replaceAll("\\[","").replaceAll("\\]","");
-
-
-        System.out.println("Arrays.toString을 활용한 배열 출력 : " + arr_toString);
-        System.out.println("Arrays.toString의 길이 : " + arr_toString.length());
-        System.out.println("Arrays.toString의 괄호 지우고 출력 : " + repl);
+    public static void main(String[] args) {
+        AudioPlayer ap = new AudioPlayer();
+        ap.play(100);
+        ap.stop();
     }
+}
+
+abstract class Player {
+    abstract void play(int pos);
+    abstract void stop();
+}
+
+class AudioPlayer extends Player {
+    void play(int pos){System.out.println(pos);}
+    void stop(){}
 }
